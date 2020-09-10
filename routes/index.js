da5846829d419ccad94dc9b4bc18f016151bf3a9
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2020-09-10 21:38:31 +0300

    add cors23

--- routes/index.js
+++ routes/index.js
@@ -23,33 +23,18 @@ const whitelist = [
 
 module.exports = function (app) {
   app.use((req, res, next) => {
-    if (whitelist.indexOf(req.headers.origin) > -1) {
-      res.header("Access-Control-Allow-Credentials", "true");
-      res.header("Access-Control-Allow-Origin", req.headers.origin);
-    } else {
-      // разрешить другим источникам отправлять неподтвержденные запросы CORS
-      res.header("Access-Control-Allow-Origin", "*");
-    }
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
     if (req.method === "OPTIONS") {
       res.header(
         "Access-Control-Allow-Methods",
-        "PUT, POST, PATCH, DELETE, GET"
+        "PUT, POST, PATCH, DELETE, GET",
       );
       return res.status(200).json({});
     }
-
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header(
-    //   "Access-Control-Allow-Headers",
-    //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    // );
-    // if (req.method === "OPTIONS") {
-    //   res.header(
-    //     "Access-Control-Allow-Methods",
-    //     "PUT, POST, PATCH, DELETE, GET"
-    //   );
-    //   return res.status(200).json({});
-    // }
     next();
   });
   app.post("/signup", signupCheck, createUser);
