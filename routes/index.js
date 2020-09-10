97966a96b3e731d3bc59d19a29c96b6d1c4042ce
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2020-09-10 21:32:47 +0300

    add cors22

--- routes/index.js
+++ routes/index.js
@@ -24,11 +24,18 @@ const whitelist = [
 module.exports = function (app) {
   app.use((req, res, next) => {
     if (whitelist.indexOf(req.headers.origin) > -1) {
-      res.set("Access-Control-Allow-Credentials", "true");
-      res.set("Access-Control-Allow-Origin", req.headers.origin);
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Origin", req.headers.origin);
     } else {
       // разрешить другим источникам отправлять неподтвержденные запросы CORS
-      res.set("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "*");
+    }
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
     }
 
     // res.header("Access-Control-Allow-Origin", "*");
